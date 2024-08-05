4f93ca695d9e2d668096219b896eb7be28c592f1
Rohit Joshi <rohitjoshi0095@gmail.com>
2024-08-05 23:18:57 +0530

    new

--- src/Components/EditCar.jsx
+++ src/Components/EditCar.jsx
@@ -5,23 +5,18 @@ import './styles/EditCar.css';
 
 const EditCar = () => {
     const [car, setCar] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
     const { id } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchCar = async () => {
             try {
-                const response = await axios.get(`https://api-front-lv6m.onrender.com/api/cars/${id}`, {
+                const response = await axios.get(`https://mern-api-5.onrender.com/api/cars/${id}`, {
                     headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
                 });
                 setCar(response.data);
-                setLoading(false);
             } catch (error) {
-                setError('Error fetching car details.');
                 console.error('Error fetching car:', error);
-                setLoading(false);
             }
         };
 
@@ -31,24 +26,20 @@ const EditCar = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.put(`https://mern-api-5.onrender.com/api/cars/${id}`, {
+            await axios.put(`https://mern-api-5.onrender.com/api/cars/${id}`, {
                 car_name: car.car_name,
                 manufacturing_year: car.manufacturing_year,
                 price: car.price
             }, {
                 headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
             });
-            console.log('Update Car Response:', response.data); // Log response for debugging
             navigate('/car-list');
         } catch (error) {
-            setError('Error updating car.');
             console.error('Error updating car:', error);
         }
     };
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
-    if (!car) return <div>No car found</div>;
+    if (!car) return <div>Loading...</div>;
 
     return (
         <div className="car-form-container">
